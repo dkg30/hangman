5f946e1245f3173428580b0687f6579e9fdc6f94
dkg30 <162714373+dkg30@users.noreply.github.com>
2024-05-17 23:53:21 +0300

    Adding play_again feature, handling unsuitable responses, optimization, calculating won games percentage

--- app.py
+++ app.py
@@ -1,36 +1,72 @@
 import random
-from data import logo, stages, word_list
+import time
+from data import logo, stages, word_list, alphabet
 
-chosen_word = random.choice(word_list)
+print(logo)
 
-display = []
+won = 0
+rounds_num = 0
 
-for i in range(len(chosen_word)):
-    display.append('_')
 
-end_of_game = False
+def hangman():
+    global won, rounds_num
+    chosen_word = random.choice(word_list)
+    display = ["_"] * len(chosen_word)
+    end_of_round = False
+    lives = 6
+    guessed_letters = []
 
-lives = 6
+    while not end_of_round:
+        guess = input('\nGuess a letter: ').lower()
+        if guess in guessed_letters:
+            print(f"You' ve already guessed {guess}\n")
+            continue
+        guessed_letters.append(guess)
+        if guess not in alphabet:
+            print("Please provided a letter.")
+            continue
+        if guess not in chosen_word:
+            print(f"You chose {guess}. It' s not in the word. You lose a life!")
+            lives -= 1
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                display[i] = guess
+        print(f"{' '.join(display)}")
+        print(stages[lives])
+        if '_' not in display and lives > 0:
+            end_of_round = True
+            print('You win!')
+            won += 1
+        elif lives == 0:
+            end_of_round = True
+            print(f'You lose!\nThe word is "{chosen_word}"')
+    rounds_num += 1
 
-guessed_letters = []
 
-while not end_of_game:
-    guess = input('Guess a letter: ').lower()
-    if guess in guessed_letters:
-        print(f"You' ve already guessed {guess}\n")
-        continue
-    guessed_letters.append(guess)
-    if guess not in chosen_word:
-        print(f"You chose {guess}. It' s not in the word. You lose a life!")
-        lives -= 1
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guess:
-            display[i] = guess
-    print(f"{' '.join(display)}")
-    print(stages[lives])
-    if '_' not in display and lives > 0:
-        end_of_game = True
-        print('You win!')
-    elif lives == 0:
-        end_of_game = True
-        print(f'You lose!\nThe word is "{chosen_word}"')
+def play_again():
+    while True:
+        answer = input("\nWould you like to play again? (Y/n) ")
+        if answer.upper() == "Y":
+            return True
+        elif answer.lower() == "n":
+            return False
+        else:
+            print("Please provide suitable answer!")
+
+
+def summary():
+    win_per_game = int(won / rounds_num * 100)
+    print(f"\nYou won {win_per_game}% ({won}/{rounds_num}) of the times!")
+
+
+def main():
+    while True:
+        hangman()
+        if not play_again():
+            break
+    summary()
+    time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
